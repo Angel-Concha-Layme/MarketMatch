f526173a5323f914db23f9cae7a4d200e99bec23
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-11-15 12:46:06 -0500

    Create PedidoFumulario.java

---
+++ src/main/java/com/tienda/pedidoscustom/dominio/Pedido/dto/PedidoFumulario.java
@@ -0,0 +1,9 @@
+package com.tienda.pedidoscustom.dominio.Pedido.dto;
+
+public record PedidoFumulario(
+                String nombre,
+                String urlProducto,
+                String urlImagen,
+                String descripcion) {
+
+}
