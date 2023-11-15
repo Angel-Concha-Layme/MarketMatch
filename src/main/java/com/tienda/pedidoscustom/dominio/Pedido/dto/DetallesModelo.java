676aa9288b4d640c5be80b13055d082c7fc75fa9
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-11-15 12:46:04 -0500

    Create DetallesModelo.java

---
+++ src/main/java/com/tienda/pedidoscustom/dominio/Pedido/dto/DetallesModelo.java
@@ -0,0 +1,5 @@
+package com.tienda.pedidoscustom.dominio.Pedido.dto;
+
+public record DetallesModelo(String nombre, String descripcion, String urlImagen, String urlProducto, String precio,
+                String fechaEntrega) {
+}
